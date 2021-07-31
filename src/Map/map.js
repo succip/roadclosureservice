6dd16bd6eb102432c58e8d5d68122f5d7d551ab7
succip <succip@gmail.com>
2021-07-31 09:43:22 -0700

    refactored constants, added sketch widget

---
+++ src/Map/map.js
@@ -0,0 +1,1 @@
+export const MAP_CENTER = [-122.849, 49.1913];
--- src/index.js
+++ src/index.js
@@ -1,15 +1,36 @@
-import Map from '@arcgis/core/Map';
-import MapView from '@arcgis/core/views/MapView';
-import Sketch from '@arcgis/core/widgets/Sketch';
-import './style.css';
+import Map from "@arcgis/core/Map";
+import MapView from "@arcgis/core/views/MapView";
+import Sketch from "@arcgis/core/widgets/Sketch";
+import GraphicsLayer from "@arcgis/core/layers/GraphicsLayer";
+import { MAP_CENTER } from "./Map/map";
+import "./style.css";
+
+let gl = new GraphicsLayer();
 
 const map = new Map({
-    basemap: 'streets',
-})
+  basemap: "streets",
+  layers: [gl],
+});
 
 const view = new MapView({
-    container: 'viewDiv',
-    map,
-    zoom: 13,
-    center: [-122.849, 49.1913]
+  container: "viewDiv",
+  map,
+  zoom: 13,
+  center: MAP_CENTER,
+});
+
+view.when(() => {
+  const sketch = new Sketch({
+    layer: gl,
+    availableCreateTools: ["polyline", "polygon"],
+    view,
+  });
+
+  view.ui.add(sketch, "top-right");
+
+  sketch.on("create", (event) => {
+    if (event.state === "complete") {
+      console.log(event.graphic.geometry);
+    }
+  });
 });
